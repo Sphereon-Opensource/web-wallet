55a7e8c5248b12b34fe428f5e3d91717e6cc77b5
Niels Klomp <nklomp@sphereon.com>
2024-04-09 09:47:25 +0200

    fix: Make sure we have stable JWKs for the same private keys (actually we didn't use the provided keys for JWKs)

--- packages/agent/src/database/contact-fixtures.ts
+++ packages/agent/src/database/contact-fixtures.ts
@@ -80,7 +80,7 @@ export async function addContacts() {
     );
     persona1.identities = [toContactIdentityDTO(persona1, identifier)];
     await agent.cmAddContact(persona1);
-    // did:jwk:eyJhbGciOiJFUzI1NiIsInVzZSI6InNpZyIsImt0eSI6IkVDIiwiY3J2IjoiUC0yNNTYiLCJ4Ijoid0kzRmhzanpQaHNkYjVSVjdQeFBIS0lmSUZDLXF5T2JYdGQxQWsyeHdHYyIsInkiOiJTbXMyUEhQX05pRW1xWEgxSzdCaTdha2h4cEJMdmp0OOVRRN2E3SkJZOUI0In0
+    // did:jwk:eyJhbGciOiJFUzI1NiIsInVzZSI6InNpZyIsImt0eSI6IkVDIiwiY3J2IjoiUC0yNTYiLCJ4IjoiSjJLUEFIQjllbUZWTk5PQ1NER2V2ZFM2cXlrUHNBLVVOM0ZfRGd5aUZrSSIsInkiOiIzSXcxZlZKakFCOS1WbjJXdklaa0s0NnNhQTZuYmh6QTBrVDZNdWV5ekNrIn0
     // did:ion:EiDktcw2GgLHQe3WehFFKciKS6rjYNEmKFIs-4_knT-Lpg:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJ3ZW5keSIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiI4N3U3NlRMVFhUT09paEo3RFZoYUloUUlmWkN4WjRja1pkNHNxaEw2OVVjIiwieSI6InlMMEFmUTdaNXBhQk9rTGh2X1h6M0QyY0oxaWdSNkVfZFViT2tSRmtZWDgifSwicHVycG9zZXMiOlsiYXV0aGVudGljYXRpb24iLCJhc3NlcnRpb25NZXRob2QiXSwidHlwZSI6IkVjZHNhU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOSJ9XX19XSwidXBkYXRlQ29tbWl0bWVudCI6IkVpRElPNlE4M1p2MkFJR09Yb2dRbHVYbEpwNTc2WVNBOWc0dVF1MzVDRVc0Y3cifSwic3VmZml4RGF0YSI6eyJkZWx0YUhhc2giOiJFaUF1eEU4RGhndmhRbzlpZG5hODgycExpY0JQZzlNYTZIS25ENWJKZWJ5ZllBIiwicmVjb3ZlcnlDb21taXRtZW50IjoiRWlDNVQ5aTVNSjVzRm9FaDg4cTdKa09qWUNQMXBEODR5ODBNSzRCZUJmeGJKZyJ9fQ
 
     const persona2 = {
@@ -99,6 +99,7 @@ export async function addContacts() {
     );
     persona2.identities = [toContactIdentityDTO(persona2, identifier)];
     await agent.cmAddContact(persona2);
+    // did:jwk:eyJhbGciOiJFUzI1NiIsInVzZSI6InNpZyIsImt0eSI6IkVDIiwiY3J2IjoiUC0yNTYiLCJ4IjoiVWtmNDNDRWJoUmVnc1hKOTZvQU0wQ2R2WnowNUt0QWtIVUVDQWt5WlBkQSIsInkiOiI1cEkwQ0pscVhKY1BxeWtLMENKVkwzNm8xMWFqamVkamQ3c0dIWVIyVHVJIn0
     // did:ion:EiAPtRGoWDBTgilTE3PhmSy0C7IxiL1-16hzv-il_l4dCQ:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJoYW5uZSIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJKaUdNcXU3VFlUdUlOTzYyV0VzQjczeTA2eFlZQkJ0clN1d0RjM3k2dFowIiwieSI6Im0tZ2xJTXhEN0Q5Z1Zvc2Q3YjdMVmNMRUxWdFVUVDZWbGN2T3cyQUx6NVkifSwicHVycG9zZXMiOlsiYXV0aGVudGljYXRpb24iLCJhc3NlcnRpb25NZXRob2QiXSwidHlwZSI6IkVjZHNhU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOSJ9XX19XSwidXBkYXRlQ29tbWl0bWVudCI6IkVpRElPNlE4M1p2MkFJR09Yb2dRbHVYbEpwNTc2WVNBOWc0dVF1MzVDRVc0Y3cifSwic3VmZml4RGF0YSI6eyJkZWx0YUhhc2giOiJFaURpWjNGRmFkUTZTQWJLUDAwZFMza05LMWRoaV9OZTNncWNoNkRFdmZvcXFnIiwicmVjb3ZlcnlDb21taXRtZW50IjoiRWlDNVQ5aTVNSjVzRm9FaDg4cTdKa09qWUNQMXBEODR5ODBNSzRCZUJmeGJKZyJ9fQ
 
     const persona3 = {
@@ -117,19 +118,20 @@ export async function addContacts() {
     );
     persona3.identities = [toContactIdentityDTO(persona3, identifier)];
     await agent.cmAddContact(persona3);
+    //did:jwk:eyJhbGciOiJFUzI1NiIsInVzZSI6InNpZyIsImt0eSI6IkVDIiwiY3J2IjoiUC0yNTYiLCJ4Ijoib1ZCaWxPcERuMTRCLVhKV0FhSzRpZzN0aURiZkM0ZW9waTZLRFgxUXVhdyIsInkiOiI0TzY5UHV1cmRJV0dyRDRmMVZIM2lUR3BZU09LUjJhQTBlMFAyYkxRbEVNIn0
     // did:ion:EiBVz8Hb_8C3BGtUcFa73jbEnSANCGJvQqGNGaTjAmD4PA:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJrZWVzIiwicHVibGljS2V5SndrIjp7ImNydiI6InNlY3AyNTZrMSIsImt0eSI6IkVDIiwieCI6IjRqMzhYUEpRWnExWW0zTkV3ZHJSZFVzY3lUdEhkM25rbEJpN1E3NkxtZTgiLCJ5IjoieU1RYkJ5dTE1d1BrWlpTTWl1R2o5VjJWbDdCeUtnM2Rab3dlVE9CcURXQSJ9LCJwdXJwb3NlcyI6WyJhdXRoZW50aWNhdGlvbiIsImFzc2VydGlvbk1ldGhvZCJdLCJ0eXBlIjoiRWNkc2FTZWNwMjU2azFWZXJpZmljYXRpb25LZXkyMDE5In1dfX1dLCJ1cGRhdGVDb21taXRtZW50IjoiRWlESU82UTgzWnYyQUlHT1hvZ1FsdVhsSnA1NzZZU0E5ZzR1UXUzNUNFVzRjdyJ9LCJzdWZmaXhEYXRhIjp7ImRlbHRhSGFzaCI6IkVpRDhFTElBSkJaMDAwMkh6OHUwWVZhWURBbkhkN2d3NTU3M0lmeW1jQnY1ZVEiLCJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaUM1VDlpNU1KNXNGb0VoODhxN0prT2pZQ1AxcEQ4NHk4ME1LNEJlQmZ4YkpnIn19
 
     const organization1 = {
-      legalName: "Rijkswaterstaat",
-      displayName: "Rijkswaterstaat",
+      legalName: "Example org",
+      displayName: "Example org",
       contactType: organizationalContactType,
-      uri: "rijkswaterstaat.nl",
+      uri: "sphereon.com",
     } as AddContactArgs;
 
     identifier = await agent.didManagerCreate(
-      existingDidConfig(DIDMethods.DID_WEB, "rws-auth", PRIVATE_DID4_KEY_HEX, {
-        traceability: true,
-        alias: "did:web:localhost:rws",
+      existingDidConfig(DIDMethods.DID_JWK, "sphereon-auth", PRIVATE_DID4_KEY_HEX, {
+        traceability: false,
+        alias: "sphereon-auth",
         type: "Secp256r1",
       }),
     );
@@ -138,6 +140,7 @@ export async function addContacts() {
       toContactIdentityDTO(organization1, identifier),
     ];
     await agent.cmAddContact(organization1);
+    // did:jwk:eyJhbGciOiJFUzI1NiIsInVzZSI6InNpZyIsImt0eSI6IkVDIiwiY3J2IjoiUC0yNTYiLCJ4IjoiWjY3eEc3UFZUUHBDdlp3UjVlR2pteHhqQjdlb2M1cWdYbm9LMloxR2R6YyIsInkiOiJ1ZkpCc3BlNTV5WkZXVWN1T21GRUMtX3NEVE1nVXRndF8tbmV2WHd4UVdZIn0
     // did:ion:EiCgT4nciFugDoVYcImyRBKAoDhTQXF3iHMyF6oS1cMqiA:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJyd3MiLCJwdWJsaWNLZXlKd2siOnsiY3J2Ijoic2VjcDI1NmsxIiwia3R5IjoiRUMiLCJ4IjoiWm5HbFAwT3F3QU9XTF9IZUlJSTZtczlLbkxCQXV2WnJtRjFPTzhnMkhBNCIsInkiOiJsd2Y4QXpmZ1lGXzU5RWRFYnhvQ1pOTXdTaVFRQ1NvY1hFQ0RtbmRfclhzIn0sInB1cnBvc2VzIjpbImF1dGhlbnRpY2F0aW9uIiwiYXNzZXJ0aW9uTWV0aG9kIl0sInR5cGUiOiJFY2RzYVNlY3AyNTZrMVZlcmlmaWNhdGlvbktleTIwMTkifV19fV0sInVwZGF0ZUNvbW1pdG1lbnQiOiJFaURJTzZRODNadjJBSUdPWG9nUWx1WGxKcDU3NllTQTlnNHVRdTM1Q0VXNGN3In0sInN1ZmZpeERhdGEiOnsiZGVsdGFIYXNoIjoiRWlDVjNUMkpuYTdMaVk4MVU2d2RhX1ZPX1VwcTU3eEtqeVh6R2JrME1xYTdUZyIsInJlY292ZXJ5Q29tbWl0bWVudCI6IkVpQzVUOWk1TUo1c0ZvRWg4OHE3SmtPallDUDFwRDg0eTgwTUs0QmVCZnhiSmcifX0
 
     const organization2 = {
@@ -250,6 +253,16 @@ function existingDidConfig(
       ],
       services,
     };
+  } else if (method === DIDMethods.DID_JWK) {
+      options = {
+          kid,
+          key: {
+              privateKeyHex: privateDIDKeyHex,
+              kid,
+              type: opts?.type ?? "Secp256r1",
+              keyType: opts?.type ?? "Secp256r1",
+          }
+      }
   } else {
     options = { services };
   }
