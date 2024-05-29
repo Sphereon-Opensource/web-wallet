a9808ad84652c0751c857bed124326d121bea1eb
sanderPostma <spostma@sphereon.com>
2024-05-29 11:49:12 +0200

    chore: lib version bumps & agent updates

--- packages/agent/src/agent.ts
+++ packages/agent/src/agent.ts
@@ -1,52 +1,52 @@
 import {createAgent, IAgentContext, IAgentPlugin, TAgent} from '@veramo/core'
 import {
-  CredentialHandlerLDLocal,
-  LdDefaultContexts,
-  MethodNames,
-  SphereonEcdsaSecp256k1RecoverySignature2020,
-  SphereonEd25519Signature2018,
-  SphereonEd25519Signature2020,
-  SphereonJsonWebSignature2020,
+    CredentialHandlerLDLocal,
+    LdDefaultContexts,
+    MethodNames,
+    SphereonEcdsaSecp256k1RecoverySignature2020,
+    SphereonEd25519Signature2018,
+    SphereonEd25519Signature2020,
+    SphereonJsonWebSignature2020,
 } from '@sphereon/ssi-sdk.vc-handler-ld-local'
-import { CredentialPlugin } from '@veramo/credential-w3c'
-import { DataStore, DataStoreORM, DIDStore, KeyStore, PrivateKeyStore } from '@veramo/data-store'
-import { DIDManager } from '@veramo/did-manager'
-import { DIDResolverPlugin } from '@veramo/did-resolver'
-import { SphereonKeyManager } from '@sphereon/ssi-sdk-ext.key-manager'
-import { SecretBox } from '@veramo/kms-local'
-import { SphereonKeyManagementSystem } from '@sphereon/ssi-sdk-ext.kms-local'
-import { getDbConnection } from './database'
+import {CredentialPlugin} from '@veramo/credential-w3c'
+import {DataStore, DataStoreORM, DIDStore, KeyStore, PrivateKeyStore} from '@veramo/data-store'
+import {DIDManager} from '@veramo/did-manager'
+import {DIDResolverPlugin} from '@veramo/did-resolver'
+import {SphereonKeyManager} from '@sphereon/ssi-sdk-ext.key-manager'
+import {SecretBox} from '@veramo/kms-local'
+import {SphereonKeyManagementSystem} from '@sphereon/ssi-sdk-ext.kms-local'
+import {getDbConnection} from './database'
 import {
-  createDidProviders,
-  createDidResolver,
-  expressBuilder,
-  getDefaultDID,
-  getDefaultKid,
-  getOrCreateDIDsFromFS,
-  getOrCreateDIDWebFromEnv,
+    createDidProviders,
+    createDidResolver,
+    expressBuilder,
+    getDefaultDID,
+    getDefaultKid,
+    getOrCreateDIDsFromFS,
+    getOrCreateDIDWebFromEnv,
 } from './utils'
 import {
-  ASSET_DEFAULT_DID_METHOD,
-  AUTHENTICATION_ENABLED,
-  AUTHENTICATION_STRATEGY,
-  AUTHORIZATION_ENABLED,
-  AUTHORIZATION_GLOBAL_REQUIRE_USER_IN_ROLES,
-  CONTACT_MANAGER_API_FEATURES,
-  DB_CONNECTION_NAME,
-  DB_ENCRYPTION_KEY,
-  DID_API_BASE_PATH,
-  DID_API_FEATURES,
-  DID_API_RESOLVE_MODE,
-  DID_WEB_SERVICE_FEATURES,
-  OID4VCI_API_BASE_URL,
-  oid4vciInstanceOpts,
-  oid4vciMetadataOpts,
-  REMOTE_SERVER_API_FEATURES,
-  STATUS_LIST_API_BASE_PATH,
-  STATUS_LIST_API_FEATURES,
-  STATUS_LIST_CORRELATION_ID,
-  VC_API_BASE_PATH,
-  VC_API_FEATURES,
+    ASSET_DEFAULT_DID_METHOD,
+    AUTHENTICATION_ENABLED,
+    AUTHENTICATION_STRATEGY,
+    AUTHORIZATION_ENABLED,
+    AUTHORIZATION_GLOBAL_REQUIRE_USER_IN_ROLES,
+    CONTACT_MANAGER_API_FEATURES,
+    DB_CONNECTION_NAME,
+    DB_ENCRYPTION_KEY,
+    DID_API_BASE_PATH,
+    DID_API_FEATURES,
+    DID_API_RESOLVE_MODE,
+    DID_WEB_SERVICE_FEATURES,
+    OID4VCI_API_BASE_URL,
+    oid4vciInstanceOpts,
+    oid4vciMetadataOpts,
+    REMOTE_SERVER_API_FEATURES,
+    STATUS_LIST_API_BASE_PATH,
+    STATUS_LIST_API_FEATURES,
+    STATUS_LIST_CORRELATION_ID,
+    VC_API_BASE_PATH,
+    VC_API_FEATURES,
 } from './environment'
 import {VcApiServer} from '@sphereon/ssi-sdk.w3c-vc-api'
 import {UniResolverApiServer} from '@sphereon/ssi-sdk.uni-resolver-registrar-api'
@@ -55,7 +55,7 @@ import {DidWebServer} from '@sphereon/ssi-sdk.uni-resolver-registrar-api/dist/di
 import {StatuslistManagementApiServer} from '@sphereon/ssi-sdk.vc-status-list-issuer-rest-api'
 import {ContactManagerApiServer} from '@sphereon/ssi-sdk.contact-manager-rest-api'
 import {ContactManager} from '@sphereon/ssi-sdk.contact-manager'
-import { ContactStore, EventLoggerStore, IssuanceBrandingStore } from '@sphereon/ssi-sdk.data-store'
+import {ContactStore, EventLoggerStore, IssuanceBrandingStore} from '@sphereon/ssi-sdk.data-store'
 import {IIssuerInstanceArgs, OID4VCIIssuer} from '@sphereon/ssi-sdk.oid4vci-issuer'
 import {OID4VCIStore} from '@sphereon/ssi-sdk.oid4vci-issuer-store'
 import {IRequiredContext, OID4VCIRestAPI} from '@sphereon/ssi-sdk.oid4vci-issuer-rest-api'
@@ -64,7 +64,7 @@ import {IOID4VCIRestAPIOpts} from '@sphereon/ssi-sdk.oid4vci-issuer-rest-api/src
 import {EventLogger} from '@sphereon/ssi-sdk.event-logger'
 import {RemoteServerApiServer} from '@sphereon/ssi-sdk.remote-server-rest-api'
 import {defaultCredentialDataSupplier} from './credentials/dataSuppliers'
-import { IssuanceBranding } from '@sphereon/ssi-sdk.issuance-branding';
+import {IssuanceBranding} from '@sphereon/ssi-sdk.issuance-branding';
 
 /**
  * Lets setup supported DID resolvers first
@@ -78,70 +78,70 @@ export const dbConnection = getDbConnection(DB_CONNECTION_NAME)
 const privateKeyStore: PrivateKeyStore = new PrivateKeyStore(dbConnection, new SecretBox(DB_ENCRYPTION_KEY))
 
 
-const maintenanceMode:boolean = process.env.RUN_MODE === 'maintenance'
+const cliMode: boolean = process.env.RUN_MODE === 'cli'
 
 /**
  * Define Agent plugins being used. The plugins come from Sphereon's SSI-SDK and Veramo.
  */
 
 const plugins: IAgentPlugin[] = [
-  new DataStore(dbConnection),
-  new DataStoreORM(dbConnection),
-  new SphereonKeyManager({
-    store: new KeyStore(dbConnection),
-    kms: {
-      local: new SphereonKeyManagementSystem(privateKeyStore),
-    },
-  }),
-  new DIDManager({
-    store: new DIDStore(dbConnection),
-    defaultProvider: `${DID_PREFIX}:${DIDMethods.DID_WEB}`,
-    providers: createDidProviders(),
-  }),
-  new DIDResolverPlugin({
-    resolver,
-  }),
-  new CredentialPlugin(),
-  new CredentialHandlerLDLocal({
-    //todo: We could add the SPHEREON contexts locally as well
-    contextMaps: [LdDefaultContexts],
-    suites: [
-      new SphereonEd25519Signature2018(),
-      new SphereonEd25519Signature2020(),
-      new SphereonJsonWebSignature2020(),
-      new SphereonEcdsaSecp256k1RecoverySignature2020(),
-    ],
-    bindingOverrides: new Map([
-      ['createVerifiableCredentialLD', MethodNames.createVerifiableCredentialLDLocal],
-      ['createVerifiablePresentationLD', MethodNames.createVerifiablePresentationLDLocal],
-    ]),
-    keyStore: privateKeyStore,
-  }),
-  new ContactManager({ store: new ContactStore(dbConnection) }),
-  new IssuanceBranding({store: new IssuanceBrandingStore(dbConnection)}),
-  new OID4VCIStore({
-    importIssuerOpts: oid4vciInstanceOpts.asArray,
-    importMetadatas: oid4vciMetadataOpts.asArray,
-  }),
-  new OID4VCIIssuer({
-    resolveOpts: {
-      resolver,
-    },
-  }),
-  new EventLogger({
-    eventTypes: [LoggingEventType.AUDIT],
-    store: new EventLoggerStore(dbConnection),
-  }),
+    new DataStore(dbConnection),
+    new DataStoreORM(dbConnection),
+    new SphereonKeyManager({
+        store: new KeyStore(dbConnection),
+        kms: {
+            local: new SphereonKeyManagementSystem(privateKeyStore),
+        },
+    }),
+    new DIDManager({
+        store: new DIDStore(dbConnection),
+        defaultProvider: `${DID_PREFIX}:${DIDMethods.DID_WEB}`,
+        providers: createDidProviders(),
+    }),
+    new DIDResolverPlugin({
+        resolver,
+    }),
+    new CredentialPlugin(),
+    new CredentialHandlerLDLocal({
+        //todo: We could add the SPHEREON contexts locally as well
+        contextMaps: [LdDefaultContexts],
+        suites: [
+            new SphereonEd25519Signature2018(),
+            new SphereonEd25519Signature2020(),
+            new SphereonJsonWebSignature2020(),
+            new SphereonEcdsaSecp256k1RecoverySignature2020(),
+        ],
+        bindingOverrides: new Map([
+            ['createVerifiableCredentialLD', MethodNames.createVerifiableCredentialLDLocal],
+            ['createVerifiablePresentationLD', MethodNames.createVerifiablePresentationLDLocal],
+        ]),
+        keyStore: privateKeyStore,
+    }),
+    new ContactManager({store: new ContactStore(dbConnection)}),
+    new IssuanceBranding({store: new IssuanceBrandingStore(dbConnection)}),
+    new OID4VCIStore({
+        importIssuerOpts: oid4vciInstanceOpts.asArray,
+        importMetadatas: oid4vciMetadataOpts.asArray,
+    }),
+    new OID4VCIIssuer({
+        resolveOpts: {
+            resolver,
+        },
+    }),
+    new EventLogger({
+        eventTypes: [LoggingEventType.AUDIT],
+        store: new EventLoggerStore(dbConnection),
+    }),
 ]
 
 /**
  * Create the agent with a context and export it, so it is available for the rest of the code, or code using this module
  */
 const agent = createAgent<TAgentTypes>({
-  plugins,
+    plugins,
 }) as TAgent<TAgentTypes>
 export default agent
-export const context: IAgentContext<TAgentTypes> = { agent }
+export const context: IAgentContext<TAgentTypes> = {agent}
 
 /**
  * Import/creates DIDs from configurations files and environment. They then get stored in the database.
@@ -153,177 +153,178 @@ await getOrCreateDIDsFromFS().catch((e) => console.log(`ERROR dids: ${e}`))
 
 const defaultDID = await getDefaultDID()
 console.log(`[DID] default DID: ${defaultDID}`)
-const defaultKid = await getDefaultKid({ did: defaultDID })
+const defaultKid = await getDefaultKid({did: defaultDID})
 console.log(`[DID] default key identifier: ${defaultKid}`)
 if (!defaultDID || !defaultKid) {
-  console.log('[DID] Agent has no default DID and Key Identifier!')
+    console.log('[DID] Agent has no default DID and Key Identifier!')
 }
 
 /**
  * Build a common express REST API configuration first, used by the exposed Routers/Services below
  */
-const expressSupport = expressBuilder().build({ startListening: false })
+const expressSupport = expressBuilder().build({startListening: false})
 
 /**
  * Authentication and authorization settings
  */
 const globalAuth = {
-  authentication: {
-    enabled: AUTHENTICATION_ENABLED,
-    strategy: AUTHENTICATION_STRATEGY,
-  },
-  authorization: {
-    enabled: AUTHORIZATION_ENABLED,
-    requireUserInRoles: AUTHORIZATION_GLOBAL_REQUIRE_USER_IN_ROLES,
-  },
-}
-
-/**
- * Enable the Verifiable Credentials API
- */
-if (VC_API_FEATURES.length > 0) {
-  new VcApiServer({
-    agent,
-    expressSupport,
-    opts: {
-      endpointOpts: {
-        globalAuth,
-        basePath: VC_API_BASE_PATH,
-      },
-      issueCredentialOpts: {
-        enableFeatures: VC_API_FEATURES,
-        proofFormat: 'jwt', //VC_API_DEFAULT_PROOF_FORMAT as ProofFormat,
-        persistIssuedCredentials: VC_API_FEATURES.includes('vc-persist'),
-      },
+    authentication: {
+        enabled: AUTHENTICATION_ENABLED,
+        strategy: AUTHENTICATION_STRATEGY,
     },
-  })
-}
-
-/**
- * Enable the Universal Resolver and Registrar Service
- */
-if (DID_API_FEATURES.length > 0) {
-  new UniResolverApiServer({
-    agent,
-    expressSupport,
-    opts: {
-      endpointOpts: {
-        globalAuth,
-        basePath: DID_API_BASE_PATH,
-        createDid: {
-          noErrorOnExistingDid: true,
-          storeSecrets: true,
-          defaultMethod: ASSET_DEFAULT_DID_METHOD,
-        },
-        resolveDid: {
-          // @ts-ignore
-          mode: DID_API_RESOLVE_MODE ?? 'hybrid',
-        },
-      },
-      enableFeatures: DID_API_FEATURES,
+    authorization: {
+        enabled: AUTHORIZATION_ENABLED,
+        requireUserInRoles: AUTHORIZATION_GLOBAL_REQUIRE_USER_IN_ROLES,
     },
-  })
 }
 
-/**
- * Allow for hosting of DID:web did.json files, to allow for easy integration of custodial DIDs
- */
-if (DID_WEB_SERVICE_FEATURES.length > 0) {
-  new DidWebServer({
-    agent,
-    expressSupport,
-    opts: {
-      globalAuth,
-      endpointOpts: {
-        enabled: DID_WEB_SERVICE_FEATURES.includes('did-web-global-resolution'),
-      },
-      enableFeatures: DID_WEB_SERVICE_FEATURES,
-    },
-  })
-}
+if (!cliMode) {
+    /**
+     * Enable the Verifiable Credentials API
+     */
+    if (VC_API_FEATURES.length > 0) {
+        new VcApiServer({
+            agent,
+            expressSupport,
+            opts: {
+                endpointOpts: {
+                    globalAuth,
+                    basePath: VC_API_BASE_PATH,
+                },
+                issueCredentialOpts: {
+                    enableFeatures: VC_API_FEATURES,
+                    proofFormat: 'jwt', //VC_API_DEFAULT_PROOF_FORMAT as ProofFormat,
+                    persistIssuedCredentials: VC_API_FEATURES.includes('vc-persist'),
+                },
+            },
+        })
+    }
 
-/**
- * Allow for statusList endpoints
- */
-if (STATUS_LIST_API_FEATURES.length > 0) {
-  new StatuslistManagementApiServer({
-    agent,
-    expressSupport,
-    opts: {
-      endpointOpts: {
-        globalAuth,
-        basePath: STATUS_LIST_API_BASE_PATH ?? '',
-        vcApiCredentialStatus: {
-          dbName: DB_CONNECTION_NAME,
-          disableGlobalAuth: true,
-          correlationId: STATUS_LIST_CORRELATION_ID,
-        },
-        getStatusList: {
-          dbName: DB_CONNECTION_NAME,
-        },
-        createStatusList: {
-          dbName: DB_CONNECTION_NAME,
-        },
-      },
-      enableFeatures: STATUS_LIST_API_FEATURES,
-    },
-  })
-}
+    /**
+     * Enable the Universal Resolver and Registrar Service
+     */
+    if (DID_API_FEATURES.length > 0) {
+        new UniResolverApiServer({
+            agent,
+            expressSupport,
+            opts: {
+                endpointOpts: {
+                    globalAuth,
+                    basePath: DID_API_BASE_PATH,
+                    createDid: {
+                        noErrorOnExistingDid: true,
+                        storeSecrets: true,
+                        defaultMethod: ASSET_DEFAULT_DID_METHOD,
+                    },
+                    resolveDid: {
+                        // @ts-ignore
+                        mode: DID_API_RESOLVE_MODE ?? 'hybrid',
+                    },
+                },
+                enableFeatures: DID_API_FEATURES,
+            },
+        })
+    }
 
-/**
- * Enable the Contact Manager API
- */
-if (CONTACT_MANAGER_API_FEATURES.length > 0) {
-  new ContactManagerApiServer({
-    opts: {
-      endpointOpts: {
-        globalAuth: {
-          authentication: {
-            enabled: false,
-          },
-        },
-      },
-      enableFeatures: CONTACT_MANAGER_API_FEATURES,
-    },
-    expressSupport,
-    agent,
-  })
-}
+    /**
+     * Allow for hosting of DID:web did.json files, to allow for easy integration of custodial DIDs
+     */
+    if (DID_WEB_SERVICE_FEATURES.length > 0) {
+        new DidWebServer({
+            agent,
+            expressSupport,
+            opts: {
+                globalAuth,
+                endpointOpts: {
+                    enabled: DID_WEB_SERVICE_FEATURES.includes('did-web-global-resolution'),
+                },
+                enableFeatures: DID_WEB_SERVICE_FEATURES,
+            },
+        })
+    }
 
-/**
- * Enable the Veramo remote server API
- */
-if (REMOTE_SERVER_API_FEATURES.length > 0) {
-  new RemoteServerApiServer({
-    agent,
-    expressSupport,
-    opts: {
-      exposedMethods: REMOTE_SERVER_API_FEATURES,
-      endpointOpts: {
-        globalAuth: {
-          authentication: {
-            enabled: false,
-          },
-        },
-      },
-    },
-  })
-}
+    /**
+     * Allow for statusList endpoints
+     */
+    if (STATUS_LIST_API_FEATURES.length > 0) {
+        new StatuslistManagementApiServer({
+            agent,
+            expressSupport,
+            opts: {
+                endpointOpts: {
+                    globalAuth,
+                    basePath: STATUS_LIST_API_BASE_PATH ?? '',
+                    vcApiCredentialStatus: {
+                        dbName: DB_CONNECTION_NAME,
+                        disableGlobalAuth: true,
+                        correlationId: STATUS_LIST_CORRELATION_ID,
+                    },
+                    getStatusList: {
+                        dbName: DB_CONNECTION_NAME,
+                    },
+                    createStatusList: {
+                        dbName: DB_CONNECTION_NAME,
+                    },
+                },
+                enableFeatures: STATUS_LIST_API_FEATURES,
+            },
+        })
+    }
 
-void OID4VCIRestAPI.init({
-  opts: {
-    baseUrl: OID4VCI_API_BASE_URL,
-    endpointOpts: {},
-  } as IOID4VCIRestAPIOpts,
-  context: context as unknown as IRequiredContext,
-  issuerInstanceArgs: {
-    credentialIssuer: OID4VCI_API_BASE_URL,
-    storeId: '_default', // TODO configurable?
-    namespace: 'oid4vci', // TODO configurable?
-  } as IIssuerInstanceArgs,
-  credentialDataSupplier: defaultCredentialDataSupplier,
-  expressSupport,
-})
-if(!maintenanceMode) {
+    /**
+     * Enable the Contact Manager API
+     */
+    if (CONTACT_MANAGER_API_FEATURES.length > 0) {
+        new ContactManagerApiServer({
+            opts: {
+                endpointOpts: {
+                    globalAuth: {
+                        authentication: {
+                            enabled: false,
+                        },
+                    },
+                },
+                enableFeatures: CONTACT_MANAGER_API_FEATURES,
+            },
+            expressSupport,
+            agent,
+        })
+    }
+
+    /**
+     * Enable the Veramo remote server API
+     */
+    if (REMOTE_SERVER_API_FEATURES.length > 0) {
+        new RemoteServerApiServer({
+            agent,
+            expressSupport,
+            opts: {
+                exposedMethods: REMOTE_SERVER_API_FEATURES,
+                endpointOpts: {
+                    globalAuth: {
+                        authentication: {
+                            enabled: false,
+                        },
+                    },
+                },
+            },
+        })
+    }
+
+    void OID4VCIRestAPI.init({
+        opts: {
+            baseUrl: OID4VCI_API_BASE_URL,
+            endpointOpts: {},
+        } as IOID4VCIRestAPIOpts,
+        context: context as unknown as IRequiredContext,
+        issuerInstanceArgs: {
+            credentialIssuer: OID4VCI_API_BASE_URL,
+            storeId: '_default', // TODO configurable?
+            namespace: 'oid4vci', // TODO configurable?
+        } as IIssuerInstanceArgs,
+        credentialDataSupplier: defaultCredentialDataSupplier,
+        expressSupport,
+    })
+
     expressSupport.start()
 }
