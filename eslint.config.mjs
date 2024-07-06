e7c6f58bb310d4dc15e571f999702a582feaac34
sanderPostma <spostma@sphereon.com>
2024-07-06 20:30:20 +0200

    chore: fixed siopv2 related bugs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,27 @@
-{
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": { "project": "./tsconfig.json" },
-  "env": { "commonjs": true, "node": true },
-  "ignorePatterns": ["node_modules", "android"],
-  "plugins": ["import", "eslint-comments"],
-  "extends": [
+import { Linter } from "eslint";
+
+const config = /** @type {Linter.FlatConfig} */ ({
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: "./tsconfig.json" },
+  env: { commonjs: true, node: true },
+  ignorePatterns: ["node_modules", "android"],
+  plugins: ["import", "eslint-comments"],
+  extends: [
     "eslint:recommended",
     "plugin:eslint-comments/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:import/typescript",
     "prettier"
   ],
-  "globals": {
-    "BigInt": true,
-    "console": true,
-    "WebAssembly": true,
-    "__DEV__": true,
-    "localStorage": true
+  globals: {
+    BigInt: true,
+    console: true,
+    WebAssembly: true,
+    __DEV__: true,
+    localStorage: true
   },
-  "rules": {
+  rules: {
     "@typescript-eslint/no-var-requires": 0,
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "eslint-comments/disable-enable-pair": ["error", { "allowWholeFile": true }],
@@ -27,4 +29,6 @@
     "import/order": ["error", { "newlines-between": "always", "alphabetize": { "order": "asc" } }],
     "sort-imports": ["error", { "ignoreDeclarationSort": true, "ignoreCase": true }]
   }
-}
+});
+
+export default config;
