ee23155b1a3c669b2e1ceeb4cd4ec71f58b083d1
sanderPostma <spostma@sphereon.com>
2024-07-27 13:54:48 +0200

    chore: fix crsGetUniqueCredentials

--- packages/web-wallet/src/agent/index.ts
+++ packages/web-wallet/src/agent/index.ts
@@ -61,6 +61,7 @@ const plugins: IAgentPlugin[] = [
       ...ebsiSupportMethods,
       ...pdManagerMethods,
       ...credentialStoreMethods,
+      'crsGetUniqueCredentials',
       ...contactManagerMethods,
       ...sphereonKeyManagerMethods,
     ],
