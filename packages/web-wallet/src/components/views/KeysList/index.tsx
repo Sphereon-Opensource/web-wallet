f399393ef29202d1c8b52422ca151b9637c7ba7e
sanderPostma <spostma@sphereon.com>
2025-10-23 13:00:16 +0200

    chore: Show kmsProvider col

--- packages/web-wallet/src/components/views/KeysList/index.tsx
+++ packages/web-wallet/src/components/views/KeysList/index.tsx
@@ -11,14 +11,14 @@ class KeyTableItem {
   type: string
   alias?: string
   identifierAlias?: string
-  kms?: string
+  kmsProvider?: string
   actions: string
 
-  constructor(data: {kid: string; type: string; alias?: string; identifierAlias?: string; kms?: string; actions: string}) {
+  constructor(data: {kid: string; type: string; alias?: string; identifierAlias?: string; kmsProvider?: string; actions: string}) {
     this.kid = data.kid
     this.type = data.type
     this.alias = data.alias
-    this.kms = data.kms
+    this.kmsProvider = data.kmsProvider
     this.identifierAlias = data.identifierAlias
     this.actions = data.actions
   }
@@ -31,7 +31,7 @@ class KeyTableItem {
       kid: key.kid,
       identifierAlias: identifier?.alias,
       type: key.type,
-      kms: key.kms,
+      kmsProvider: key.meta?.providerName,
       //todo
       /**
        * this should come from the key itself. probably we need to revisit this during CWALL-211. according to Niels: Not for every implementation a kid will be equal to an alias
@@ -136,7 +136,7 @@ const KeysList: FC<Props> = ({allowAddKey = true}: Props): ReactElement => {
       },
     },
     {
-      accessor: 'kms',
+      accessor: 'kmsProvider',
       label: translate('key_fields_kms_provider'),
       type: TableCellType.TEXT,
       columnOptions: {
