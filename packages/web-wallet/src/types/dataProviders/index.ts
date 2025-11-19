c13ba1d145983d08e8866ffe4ea875d2e1fb6793
A.G.J. Cate <brummos@gmail.com>
2025-11-19 14:05:41 +0100

    chore: cleanup

--- packages/web-wallet/src/types/dataProviders/index.ts
+++ packages/web-wallet/src/types/dataProviders/index.ts
@@ -9,3 +9,5 @@ export enum DataProvider {
   QUERIES = 'presentationDefinitions',
   CREDENTIAL_DESIGNS = 'credential_designs',
 }
+
+export type DcqlClaim = NonNullable<DcqlCredentialQuery['claims']>[number] // TODO find a better location for this
