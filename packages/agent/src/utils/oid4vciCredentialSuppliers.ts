13e714b2ab0355ecbf01140af45c3d4baa8de078
sanderPostma <spostma@sphereon.com>
2025-11-26 00:29:21 +0100

    chore: temp workaround to skip credentialSupplierConfig.template_mappings

--- packages/agent/src/utils/oid4vciCredentialSuppliers.ts
+++ packages/agent/src/utils/oid4vciCredentialSuppliers.ts
@@ -99,6 +99,14 @@ class TemplateCredentialDataSupplier {
       credential = credentialPayload as ICredential
     }
 
+    // FIXME!!! Temp hack to see that the credential data is probably coming from the web wallet's JSON schema generator already providing a valid credential payload
+    if(credential && ('disclosureFrame' in credential || 'statusList' in credential)) {
+      return Promise.resolve({
+        format: args.format,
+        credential: credential,
+      } as unknown as CredentialDataSupplierResult)
+    }
+
     const credentialSupplierConfig = args.credentialSupplierConfig as CredentialSupplierConfigWithTemplateSupport
     const requestedConfigId = (credentialRequest as CredentialRequestV1_0_15).credential_configuration_id
     if (credentialSupplierConfig.template_mappings) {
