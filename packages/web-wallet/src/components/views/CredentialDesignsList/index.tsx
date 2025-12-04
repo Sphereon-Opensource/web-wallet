440ec61327b6f910192115b446c0f189b833b8c8
A.G.J. Cate <brummos@gmail.com>
2025-12-04 13:27:33 +0100

    chore: added initial support for pagination

--- packages/web-wallet/src/components/views/CredentialDesignsList/index.tsx
+++ packages/web-wallet/src/components/views/CredentialDesignsList/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC, ReactElement} from 'react'
+import React, {ChangeEvent, FC, ReactElement, useState} from 'react'
 import {HttpError, useDelete, useList, useNavigation, useTranslate} from '@refinedev/core'
 import {ColumnHeader, Row, SSITableView, TableCellType} from '@sphereon/ui-components.ssi-react'
 import {ButtonIcon} from '@sphereon/ui-components.core'
@@ -13,6 +13,8 @@ const CredentialDesignsList: FC<Props> = (props: Props): ReactElement => {
   const translate = useTranslate()
   const {mutateAsync: deleteCredential} = useDelete<CredentialDesignTableItem, HttpError>()
   const {create, show} = useNavigation()
+  const [current, setCurrent] = useState<number>(1)
+  const [pageSize, setPageSize] = useState<number>(10)
 
   const {
     data: credentialDesigns,
@@ -22,7 +24,8 @@ const CredentialDesignsList: FC<Props> = (props: Props): ReactElement => {
   } = useList<CredentialDesignTableItem, HttpError>({
     resource: DataResource.CREDENTIAL_DESIGNS,
     pagination: {
-      pageSize: 1000,
+      pageSize,
+      current,
       mode: 'server',
     },
     sorters: [
@@ -48,7 +51,7 @@ const CredentialDesignsList: FC<Props> = (props: Props): ReactElement => {
     create(DataResource.CREDENTIAL_DESIGNS)
   }
 
-  // TODO SSISDK-87 add support for additional data
+  // TODO SSISDK-86 add support for additional data
   const columns: ColumnHeader<CredentialDesignTableItem>[] = [
     {
       accessor: 'name',
@@ -97,12 +100,40 @@ const CredentialDesignsList: FC<Props> = (props: Props): ReactElement => {
     return <div>{translate('data_provider_loading_message')}</div>
   }
 
+  const designsData = credentialDesigns?.data ?? []
+  const totalDesigns = credentialDesigns?.total ?? 0
+
+  const onPageChange = (_event: ChangeEvent<unknown>, page: number) => {
+    setCurrent(page)
+  }
+  const onPageChangeKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      const input = event.target as HTMLInputElement
+      let goToPage = Number(input.value)
+
+      if (!isNaN(goToPage) && goToPage >= 1 && goToPage <= Math.ceil(totalDesigns / pageSize)) {
+        setCurrent(goToPage)
+      } else {
+        console.error('Invalid page number')
+      }
+    }
+  }
+
   return <div>
     <SSITableView
-      data={credentialDesigns.data}
+      data={designsData}
       columns={columns}
       actions={buildActionList()}
       onRowClick={onShow}
+      // TODO SSISDK-86 enable when data provider supports pagination
+      // pagination={{
+      //   page: current,
+      //   count: Math.ceil(totalDesigns / pageSize),
+      //   onChange: onPageChange,
+      //   goToInputId: 'custom-goToInput',
+      //   containerStyle: {marginTop: '20px'},
+      //   onKeyDown: onPageChangeKeyDown,
+      // }}
     />
   </div>
 }
