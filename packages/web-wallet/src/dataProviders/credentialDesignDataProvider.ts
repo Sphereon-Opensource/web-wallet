90e3540ba892f8c8b8958d7ed66f41165a7061db
A.G.J. Cate <brummos@gmail.com>
2025-12-10 12:54:28 +0100

    chore: added toDTO

--- packages/web-wallet/src/dataProviders/credentialDesignDataProvider.ts
+++ packages/web-wallet/src/dataProviders/credentialDesignDataProvider.ts
@@ -190,7 +190,7 @@ export const credentialDesignDataProvider = (): DataProvider => ({
       )
     )
 
-    const data: TData[] = results.map((result: any) => result.data)
+    const data: TData[] = results.map((result: any) => new CredentialDesignEntity(result).asDTO())
 
     return { data }
   },
